5c7a8df9eb676a9fcbcaaa7423ac2e01cfc7c16f
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-05-09 23:03:19 +0900

    main.tsにpiniaを追加

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css'
 import { router } from '@/modules/router'
+import { createPinia } from 'pinia'
 
 createApp(App)
   .use(router)
+  .use(createPinia())
   .mount('#app')
