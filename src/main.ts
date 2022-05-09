aed207bde653fff3bd6dd5f221dcf02a89c489ee
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-05-09 21:15:17 +0900

    router configを記述

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css'
+import { router } from '@/modules/router'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
