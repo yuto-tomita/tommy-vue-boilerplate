afc8265ebb95a7048c9b95f6d69a737bb27b493b
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-05-10 21:43:01 +0900

    storeのモック作成

---
+++ src/modules/store/index.ts
@@ -0,0 +1,39 @@
+import { defineStore } from 'pinia'
+
+interface Todo {
+	id: number,
+	title: string
+}
+
+export const useStore = defineStore({
+  id: 'todos',
+  state: (): {
+		todos: Todo[]
+	} => {
+    return {
+      todos: [
+        {
+          id: 0,
+          title: 'pinia'
+        },
+        {
+          id: 1,
+          title: 'vue-router'
+        },
+        {
+          id: 2,
+          title: 'tailwindcss'
+        },
+        {
+          id: 3,
+          title: 'vite'
+        }
+      ]
+    }
+  },
+  getters: {
+    getTodos: (state) => state.todos
+  },
+  actions: {
+  }
+})
